d8107fc53c2b80f1b43c45c0dfd08224af456b01
Gustavo Martins Pereira <gustavo.martins.pereira.main@gmail.com>
2026-03-22 14:42:46 -0300

    ADD - Spawn from Sides

--- Assets/Scripts/DestroyOutOfBounds.cs
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -2,8 +2,9 @@ using UnityEngine;
 
 public class DestroyOutOfBounds : MonoBehaviour
 {
-    private float outOfTopBoundLimit = 25f;
-    private float outOfBottomBoundLimit = -10f;
+    private readonly float outOfTopBoundLimit = 25f;
+    private readonly float outOfBottomBoundLimit = -10f;
+    private readonly float outOfSideBoundLimit = 40f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -14,7 +15,13 @@ public class DestroyOutOfBounds : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(transform.position.z > outOfTopBoundLimit || transform.position.z < outOfBottomBoundLimit)
+        if(
+            // Top Bound
+            transform.position.z > outOfTopBoundLimit || transform.position.z < outOfBottomBoundLimit
+            ||
+            // Side Bound
+            transform.position.x < -outOfSideBoundLimit || transform.position.x > outOfSideBoundLimit
+        )
         {
             Destroy(gameObject);
 
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -5,8 +5,11 @@ public class SpawnManager : MonoBehaviour
 {
     public GameObject[] spawnPrefabs;
 
-    private readonly float spawnRangeX = 15f;
-    private readonly float spawnPositionZ = 25f;
+    private readonly float topSpawnRangeX = 15f;
+    private readonly float topSpawnPositionZ = 25f;
+
+    private readonly float sideSpawnRangeZ = 10f;
+    private readonly float sideSpawnPositionX = 25f;
 
     private readonly int delay = 2;
     private readonly int interval = 1;
@@ -25,9 +28,28 @@ public class SpawnManager : MonoBehaviour
 
     void SpawnAnimal()
     {
-        int randomAnimal = Random.Range(0, spawnPrefabs.Length);
-        Vector3 spawnPosition = new(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPositionZ);
+        bool isSpawnTop = Random.Range(1, 3) % 2 == 0;
 
-        Instantiate(spawnPrefabs[randomAnimal], spawnPosition, spawnPrefabs[randomAnimal].transform.rotation);
+        int randomAnimal = Random.Range(0, spawnPrefabs.Length);
+        if (isSpawnTop)
+        {
+            Vector3 spawnPosition = new(Random.Range(-topSpawnRangeX, topSpawnRangeX), 0, topSpawnPositionZ);
+
+            Instantiate(spawnPrefabs[randomAnimal], spawnPosition, spawnPrefabs[randomAnimal].transform.rotation);
+        }
+        else
+        {
+            // Position
+            bool isLeftSide = Random.Range(1, 3) % 2 == 0;
+            int sideSpawnRangeZOffset = 2;
+            Vector3 spawnPosition = new(isLeftSide ? -sideSpawnPositionX : sideSpawnPositionX, 0, Random.Range(sideSpawnRangeZOffset, sideSpawnRangeZOffset + sideSpawnRangeZ));
+
+            // Animal Rotation
+            Vector3 currentEuler = spawnPrefabs[randomAnimal].transform.rotation.eulerAngles;
+            float targetY = isLeftSide ? 90f : -90f;
+            Quaternion spawnRotation = Quaternion.Euler(currentEuler.x, targetY, currentEuler.z);
+
+            Instantiate(spawnPrefabs[randomAnimal], spawnPosition, spawnRotation);
+        }
     }
 }
